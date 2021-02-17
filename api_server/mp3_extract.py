8f5f5eea101ce4be133d0428d9b6a730fcef40d4
skyriver228 <63352107+skyriver228@users.noreply.github.com>
2021-02-17 23:06:07 +0900

    making folder and variable
    기존의 코드에서 close를 해주지 않으면 ffmpeg가 계속해서 돌아가서 닫아주었습니다. 변수명을 따로 빼고, 폴더도 일단 따로 분리해두었습니다. 다만, 주피터 노트북에서는 잘 실행되는데 vsc에서는 상대경로를 찾지 못하는 에러가 있어서 해결방안을 찾아보는 중입니다.

--- api_server/mp3_extract.py
+++ api_server/mp3_extract.py
@@ -1,6 +1,12 @@
-# 모듈 로딩 후 오디오 추출
+# 모듈 로딩(version 1.0.3) 후 오디오 추출
 import moviepy.editor as mp
-#(로컬 기준) 같은 프로젝트 파일 내에 있는 mp4 영상을 clip에 저장
-clip = mp.VideoFileClip("고봉밥_팀 소개 영상.mp4")
-#audio를 사용해서 mp4 파일(clip)을 mp3 파일로 다시 저장
-clip.audio.write_audiofile("고봉밥_팀 소개 음성.mp3")
+
+# 추후에 쉽게 변경할 수 있도록 파일명 상대경로로 저장
+INPUT_VIDEO = "./input_video/고봉밥_팀 소개 영상.mp4"
+OUTPUT_AUDIO = "./output_audio/고봉밥_팀 소개 음성.mp3"
+
+# (로컬 기준) 같은 프로젝트 파일 내에 있는 mp4 영상을 clip에 저장
+clip = mp.VideoFileClip(INPUT_VIDEO)
+# audio를 사용해서 mp4 파일(clip)을 mp3 파일로 다시 저장
+clip.audio.write_audiofile(OUTPUT_AUDIO)
+clip.close()
