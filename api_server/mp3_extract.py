435a2daaffc42ce97784bbc0eb084d7ec7d49a2b
skyriver228 <63352107+skyriver228@users.noreply.github.com>
2021-02-18 19:09:40 +0900

    path_error solved
    기본 경로가 api_server가 아니라 Learn_on에 있어서 발생한 문제였습니다.

--- api_server/mp3_extract.py
+++ api_server/mp3_extract.py
@@ -2,8 +2,8 @@
 import moviepy.editor as mp
 
 # 추후에 쉽게 변경할 수 있도록 파일명 상대경로로 저장
-INPUT_VIDEO = "./input_video/고봉밥_팀 소개 영상.mp4"
-OUTPUT_AUDIO = "./output_audio/고봉밥_팀 소개 음성.mp3"
+INPUT_VIDEO = "./api_server/input_video/example.MP4"
+OUTPUT_AUDIO = "./api_server/output_audio/example.mp3"
 
 # (로컬 기준) 같은 프로젝트 파일 내에 있는 mp4 영상을 clip에 저장
 clip = mp.VideoFileClip(INPUT_VIDEO)
