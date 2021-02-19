093e9545fea502af6718112298990ac4273ab087
bob.siunn <60945415+bobsiunn@users.noreply.github.com>
2021-02-20 00:47:05 +0900

    Add files via upload

--- api_server/transribe.py
+++ api_server/transribe.py
@@ -1,29 +1,67 @@
 from __future__ import print_function
+import pandas as pd
 import time
 import boto3
-import pandas as pd
 
 transcribe = boto3.client('transcribe')
-job_uri = "https://kpmg-gobongbob.s3.us-east-2.amazonaws.com/input_video/example.mp4"
-job_name = job_uri[62:].rstrip(".mp4")
 
-transcribe.start_transcription_job(
-    TranscriptionJobName=job_name,
-    Media={'MediaFileUri': job_uri},
-    MediaFormat='mp4',
-    LanguageCode='en-US'
-)
+def check_job_name(job_name):
+    job_verification = True
+    # all the transcriptions
+    existed_jobs = transcribe.list_transcription_jobs()
+    for job in existed_jobs['TranscriptionJobSummaries']:
+        if job_name == job['TranscriptionJobName']:
+            job_verification = False
+            break
+    if job_verification == False:
+        command = input(job_name + " has existed. \n Do you want to override the existed job (Y/N): ")    
+    
+        if command.lower() == "y" or command.lower() == "yes":                
+            transcribe.delete_transcription_job(TranscriptionJobName=job_name)
+        elif command.lower() == "n" or command.lower() == "no":      
+            ob_name = input("Insert new job name? ")      
+            check_job_name(job_name)
+        else:
+            print("Input can only be (Y/N)")
+            command = input(job_name + " has existed. \nDo you want to override the existed job (Y/N): ")
+            check_job_name(job_name)
 
-while True:
-    result = transcribe.get_transcription_job(TranscriptionJobName=job_name)
-    if result['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
-        break
-    print("Not ready yet...")
-    time.sleep(5)
+    return job_name
 
-if result['TranscriptionJob']['TranscriptionJobStatus'] == "COMPLETED":
-    data = pd.read_json(result['TranscriptionJob']
-                        ['Transcript']['TranscriptFileUri'])
 
-print(data['results'][1][0]['transcript'])
->>>>>> > upstream/main
+def amazon_transcribe(audio_file_name):
+    job_uri = "https://kpmg-gobongbob.s3.us-east-2.amazonaws.com/input_video/example.mp4"
+    job_name = (audio_file_name.split('.')[0]).replace(" ", "")
+    file_format = audio_file_name.split('.')[1]
+
+    job_name = check_job_name(job_name)
+
+    transcribe.start_transcription_job(
+        TranscriptionJobName=job_name,
+        Media={'MediaFileUri': job_uri},
+        MediaFormat = file_format,
+        LanguageCode='en-US'
+    )
+  
+    while True:
+        result = transcribe.get_transcription_job(TranscriptionJobName=job_name)
+        if result['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
+            break
+        print("Not ready yet...")
+        time.sleep(5)
+
+    #print(result)
+
+    if result['TranscriptionJob']['TranscriptionJobStatus'] == "COMPLETED":
+        data = pd.read_json(result['TranscriptionJob']['Transcript']['TranscriptFileUri'])
+    #print(data['results'][1][0]['transcript'])
+    #time_tag = []
+    #for i in range(len(data['results'][0])):
+        #if('start_time' in data['results'][0][i] and 'end_time' in data['results'][0][i]):
+            #time_tag.append([data['results'][0][i]['start_time'], data['results'][0][i]['end_time']])
+    #print(time_tag)
+    #print(data['results'][0])
+    return([data['results'][1][0]['transcript'], data['results'][0]])
+    #print(data['results'][1][0]['transcript'])
+
+transcribe_result = amazon_transcribe("kpmg_transcript12.mp4")
