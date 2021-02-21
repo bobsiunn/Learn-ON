7961e36efda2b33d8e98f854993f302f12a51d41
hustle-dev <dlwoabsdk@gmail.com>
2021-02-21 17:02:49 +0900

    edit file

---
+++ front_server/frontend/routes/index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router();
+
+const multer = require("multer");
+const fs = require('fs');
+const AWS = require('aws-sdk');
+const BUCKET_NAME = 'jm-tests3';
+const s3 = new AWS.S3();
+
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/"); //uploads라는 폴더에 file을 저장
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}_${file.originalname}`);
+    },
+    fileFilter: (req, file, cb) => {
+        const ext = path.extname(file.originalname);
+        if (ext !== ".mp4") {
+            return cb(res.status(400).end("only mp4 is allowd"), false);
+        }
+        cb(null, true);
+    }
+});
+const upload = multer({ storage: storage }).single("file"); //파일하나만업로드하겠다는의미
+
+//=================================
+//             Video
+//=================================
+
+router.post("/uploadfiles", (req, res) => {
+    //비디오를 서버에 저장한다.
+    upload(req, res, (err) => {
+        if (err) {
+            return res.json({ success: false, err });
+        }
+        console.log(res.req.file.path);
+        uploadFile(res.req.file.filename);
+        return res.json({
+            success: true,
+            url: res.req.file.path, //파일을 저장하게되면 uploads폴더안에 저장되게되는데 그경로를 보내줌
+            fileName: res.req.file.filename,
+        });
+    });
+});
+const uploadFile = (fileName) => {
+    const fileContent = fs.readFileSync(`./uploads/${fileName}`);
+    const params = {
+        Bucket: BUCKET_NAME,
+        Key: fileName, // File name you want to save as in S3
+        Body: fileContent
+    };
+    s3.upload(params, function(err, data) {
+        if (err) { throw err; }
+        console.log(`File uploaded successfully. ${data.Location}`);
+    });
+};
+
+module.exports = router;
---
+++ front_server/frontend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port =process.env.PORT || 3001;
+const route = require('./routes/index');
+const { uploadImageMulterMiddleware } = require("./middleware");
+const cors = require('cors');
+
+app.use(cors());
+
+
+app.use(bodyParser.json());
+app.use("/api/video", require("./routes/index"));
+
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+app.listen(port, ()=>{
+    console.log(`express is running on ${port}`);
+})
